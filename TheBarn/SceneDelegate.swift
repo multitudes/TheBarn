c25dbcc9b7381af8dac5a7826b3d0bae2a79cd0b
laurent <wrmultitudes@gmail.com>
2020-04-28 20:40:28 +0200

    add icons

--- TheBarn/SceneDelegate.swift
+++ TheBarn/SceneDelegate.swift
@@ -14,12 +14,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let homeNC = UINavigationController(rootViewController: HomeVC())
+        let favoritesNC = UINavigationController(rootViewController: FavoritesListVC())
+        
+        let tabbar = UITabBarController()
+        tabbar.viewControllers = [homeNC, favoritesNC]
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabbar
+        window?.makeKeyAndVisible()
     }
 
+    func createHomeNC() -> UINavigationController {
+        let homeVC = HomeVC()
+        homeVC.title = "Home"
+        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
+        return UINavigationController(rootViewController: homeVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
